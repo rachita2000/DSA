bae572c7dd1c5c0779066086e592a052fe6ea34d
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-21 18:54:08 +0530

    recursion1 updated

--- recursion1/Multiplication.java
+++ recursion1/Multiplication.java
@@ -3,11 +3,10 @@ package recursion1;
 public class Multiplication {
 
 	public static int multiplyTwoIntegers(int m, int n){
-		// Write your code here
-	if(m==0 ||n==0)
-		return 0;
-	
-	
-	return (multiplyTwoIntegers(m-1,n)+n);
+		
+		if(m==0 ||n==0)
+			return 0;
+		
+		return (multiplyTwoIntegers(m-1,n)+n);
 	}
 }
