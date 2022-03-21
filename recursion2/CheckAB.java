14c11838a886dd7149fa05a49133af2e229dd206
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-21 18:54:26 +0530

    recursion2 updated

--- recursion2/CheckAB.java
+++ recursion2/CheckAB.java
@@ -3,7 +3,7 @@ package recursion2;
 public class CheckAB {
 
 	public static boolean checkAB(String input) {
-		// Write your code here
+		
 		if(input.length()==0)
             return true;
         if(input.charAt(0)=='a')
